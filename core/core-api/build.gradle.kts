8c3356dd2885a509a119c9a869b45ff734be6f72
alliwannadev <alliwannadev@gmail.com>
2025-05-01 03:43:51 +0900

    feat: P6Spy 설정 (#1)

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -4,5 +4,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
 
+    // P6Spy
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.2")
+
     implementation(project(":supports:snowflake"))
 }
