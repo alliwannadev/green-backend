3094cc6a5a3d5b190a5dc62a53490317f88ef8b3
alliwannadev <alliwannadev@gmail.com>
2025-05-01 15:07:10 +0900

    feat: User API 추가 (#2) * 유저 본인 정보를 조회할 수 있는 API 추가

---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/user/controller/UserApiPaths.java
@@ -0,0 +1,13 @@
+package alliwannadev.shop.domain.user.controller;
+
+import alliwannadev.shop.common.constant.ApiPaths;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class UserApiPaths {
+
+    private static final String V1_USERS = ApiPaths.V1 + "/users";
+
+    public static final String V1_USERS_ME = V1_USERS + "/me";
+}
