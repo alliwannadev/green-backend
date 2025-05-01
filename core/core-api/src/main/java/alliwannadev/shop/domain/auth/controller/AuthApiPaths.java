846475483a7e7a6f4fa04b89e437130f16a679e8
alliwannadev <alliwannadev@gmail.com>
2025-05-01 15:17:24 +0900

    feat: Auth Api 구현 (#2) * 회원가입/로그인 API 구현

---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/auth/controller/AuthApiPaths.java
@@ -0,0 +1,14 @@
+package alliwannadev.shop.domain.auth.controller;
+
+import alliwannadev.shop.common.constant.ApiPaths;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AuthApiPaths {
+
+    private static final String V1_AUTH = ApiPaths.V1 + "/auth";
+
+    public static final String V1_SIGN_IN = V1_AUTH + "/sign-in";
+    public static final String V1_SIGN_UP = V1_AUTH + "/sign-up";
+}
---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/auth/controller/AuthControllerV1.java
@@ -0,0 +1,45 @@
+package alliwannadev.shop.domain.auth.controller;
+
+import alliwannadev.shop.common.dto.OkResponse;
+import alliwannadev.shop.common.dto.TokenInfo;
+import alliwannadev.shop.domain.auth.controller.dto.request.SignInRequestV1;
+import alliwannadev.shop.domain.auth.controller.dto.request.SignUpRequestV1;
+import alliwannadev.shop.domain.auth.controller.dto.response.TokenResponseV1;
+import alliwannadev.shop.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+@RestController
+public class AuthControllerV1 {
+
+    private final AuthService authService;
+
+    @PostMapping(AuthApiPaths.V1_SIGN_UP)
+    public OkResponse<Void> signUp(
+            @Valid @RequestBody SignUpRequestV1 signUpRequestV1
+    ) {
+        authService.signUp(signUpRequestV1);
+        return OkResponse.of("회원가입을 성공적으로 완료하였습니다.");
+    }
+
+    @PostMapping(AuthApiPaths.V1_SIGN_IN)
+    public OkResponse<TokenResponseV1> signIn(
+            @Valid @RequestBody SignInRequestV1 signInRequestV1
+    ) {
+        TokenInfo tokenInfo = authService.signIn(signInRequestV1);
+        return OkResponse.of(
+                TokenResponseV1.of(
+                        tokenInfo.grantType(),
+                        tokenInfo.accessToken()
+                )
+        );
+    }
+}
