ee036e2a43d9c8de7f4effebb34deacd27fda3f6
alliwannadev <alliwannadev@gmail.com>
2025-05-03 23:52:30 +0900

    test: 통합 테스트 작성 - 회원 ME API (#2)

---
+++ core/core-api/src/test/java/alliwannadev/shop/domain/user/controller/UserApiV1Test.java
@@ -0,0 +1,47 @@
+package alliwannadev.shop.domain.user.controller;
+
+import alliwannadev.shop.common.IntegrationTest;
+import alliwannadev.shop.common.TestContainers;
+import alliwannadev.shop.domain.auth.helper.TestAuthDbHelper;
+import alliwannadev.shop.domain.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("통합 테스트 - 회원 API V1")
+@IntegrationTest
+class UserApiV1Test extends TestContainers {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired TestAuthDbHelper testAuthDbHelper;
+
+    @DisplayName("[API][GET][SUCCESS] 회원 ME API를 호출하면 회원 본인 정보를 반환한다.")
+    @Test
+    void willSucceedIfCallUsersMeApiWithValidParams() throws Exception {
+        // Given
+        User defaultTestUser = testAuthDbHelper.createDefaultTestUserIfNotExists();
+        String accessToken = testAuthDbHelper.getDefaultToken();
+
+        // When & Then
+        mockMvc
+                .perform(
+                        get(UserApiPaths.V1_USERS_ME)
+                                .contentType(APPLICATION_JSON)
+                                .header("Authorization", accessToken)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+                .andExpect(jsonPath("$.data.userId").value(defaultTestUser.getUserId()))
+                .andExpect(jsonPath("$.data.email").value(defaultTestUser.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(defaultTestUser.getName()))
+                .andExpect(jsonPath("$.data.phone").value(defaultTestUser.getPhone()))
+                .andExpect(jsonPath("$.data.imageUrl").value(defaultTestUser.getImageUrl()));
+    }
+}
