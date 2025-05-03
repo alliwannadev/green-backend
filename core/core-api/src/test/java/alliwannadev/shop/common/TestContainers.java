f9ecf0328220a2f586c717f4be9839089bd995fe
alliwannadev <alliwannadev@gmail.com>
2025-05-03 23:46:39 +0900

    chore: 테스트 컨테이너 관련 문제 해결 (#2) * 각 테스트 클래스 마다 도커 컨테이너가 새롭게 생성되는 문제를 해결함

--- core/core-api/src/test/java/alliwannadev/shop/common/TestContainers.java
+++ core/core-api/src/test/java/alliwannadev/shop/common/TestContainers.java
@@ -3,24 +3,25 @@ package alliwannadev.shop.common;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class TestContainers {
 
     private static final String MYSQL_DATABASE_NAME = "green";
     private static final String MYSQL_USERNAME = "green_user";
     private static final String MYSQL_PASSWORD = "123456";
 
-    @Container
     static MySQLContainer<?> MYSQL_CONTAINER =
             new MySQLContainer<>("mysql:8.4.5")
                     .withConfigurationOverride("testcontainers/conf")
                     .withUsername(MYSQL_USERNAME)
                     .withPassword(MYSQL_PASSWORD)
                     .withDatabaseName(MYSQL_DATABASE_NAME)
-                    .withInitScript("testcontainers/sql/init.sql");
+                    .withInitScript("testcontainers/sql/init.sql")
+                    .withReuse(true);
+
+    static {
+        MYSQL_CONTAINER.start();
+    }
 
     @DynamicPropertySource
     static void overrideProps(DynamicPropertyRegistry registry) {
