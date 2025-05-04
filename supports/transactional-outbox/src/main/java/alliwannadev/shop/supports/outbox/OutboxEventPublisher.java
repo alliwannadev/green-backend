8a387d411f52272eff33a1767028cac81c890ef7
alliwannadev <alliwannadev@gmail.com>
2025-05-05 00:09:28 +0900

    feat: Transactional Outbox 모듈 구현 (#3)

---
+++ supports/transactional-outbox/src/main/java/alliwannadev/shop/supports/outbox/OutboxEventPublisher.java
@@ -0,0 +1,37 @@
+package alliwannadev.shop.supports.outbox;
+
+import alliwannadev.shop.supports.event.Event;
+import alliwannadev.shop.supports.event.EventPayload;
+import alliwannadev.shop.supports.event.EventType;
+import alliwannadev.shop.supports.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class OutboxEventPublisher {
+
+    private final Snowflake snowflake = new Snowflake(1L, 1L);
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public void publish(
+            EventType type,
+            EventPayload payload,
+            Long shardKey
+    ) {
+        Outbox outbox = Outbox.create(
+                snowflake.nextId(),
+                shardKey,
+                type,
+                Event.of(
+                        snowflake.nextId(),
+                        shardKey,
+                        type,
+                        payload
+                ).toJson()
+        );
+
+        applicationEventPublisher.publishEvent(OutboxEvent.of(outbox));
+    }
+}
