ac7331ef26e7fc83636969bef2d0767eea587a84
alliwannadev <alliwannadev@gmail.com>
2025-05-09 20:30:26 +0900

    feat: 상품 단건 및 목록 조회 API 추가 (#4)

--- core/core-api/src/main/java/alliwannadev/shop/domain/product/controller/ProductApiV1.java
+++ core/core-api/src/main/java/alliwannadev/shop/domain/product/controller/ProductApiV1.java
@@ -1,14 +1,20 @@
 package alliwannadev.shop.domain.product.controller;
 
-import alliwannadev.shop.common.dto.OkResponse;
+import alliwannadev.shop.common.dto.*;
 import alliwannadev.shop.domain.product.controller.dto.CreateProductRequestV1;
+import alliwannadev.shop.domain.product.controller.dto.GetProductListRequestV1;
+import alliwannadev.shop.domain.product.controller.dto.GetProductResponseV1;
 import alliwannadev.shop.domain.product.service.ProductService;
+import alliwannadev.shop.domain.product.service.dto.GetProductResult;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Validated
 @RequiredArgsConstructor
@@ -23,5 +29,53 @@ public class ProductApiV1 {
     ) {
         productService.createOne(createProductRequestV1.toDto());
         return OkResponse.of("상품 등록을 완료했습니다.");
+    }
+
+    @GetMapping(ProductApiPaths.V1_PRODUCTS)
+    public OkResponse<PaginationResponse<GetProductResponseV1>> getAll(
+            @Valid @ModelAttribute PaginationRequest customPageRequestV1,
+            @Valid @ModelAttribute GetProductListRequestV1 getProductListRequestV1
+    ) {
+        Page<GetProductResult> searchProductResultPage = productService.getAll(
+                getProductListRequestV1.toDto(),
+                customPageRequestV1.getPageable()
+        );
+
+        return OkResponse.of(
+                PaginationResponse.of(
+                        searchProductResultPage.map(GetProductResponseV1::fromDto),
+                        customPageRequestV1
+                )
+        );
+    }
+
+    @GetMapping(ProductApiPaths.V1_PRODUCTS_INFINITE_SCROLL)
+    public OkResponse<InfiniteScrollResponse<GetProductResponseV1>> getAllInfiniteScroll(
+            @NotEmpty @RequestParam String categoryPath,
+            @Valid @ModelAttribute InfiniteScrollRequest infiniteScrollRequest
+    ) {
+        List<GetProductResponseV1> result = productService
+                .getAllInfiniteScroll(
+                        categoryPath,
+                        infiniteScrollRequest.toCond()
+                )
+                .stream()
+                .map(GetProductResponseV1::fromDto)
+                .toList();
+
+        return OkResponse.of(
+                InfiniteScrollResponse.of(
+                        result,
+                        infiniteScrollRequest.pageSize()
+                )
+        );
+    }
+
+    @GetMapping(ProductApiPaths.V1_PRODUCTS_BY_PRODUCT_ID)
+    public OkResponse<GetProductResponseV1> getOneByProductId(
+            @PathVariable @Positive Long productId
+    ) {
+        GetProductResult result = productService.getOneByProductId(productId);
+        return OkResponse.of(GetProductResponseV1.fromDto(result));
     }
 }
