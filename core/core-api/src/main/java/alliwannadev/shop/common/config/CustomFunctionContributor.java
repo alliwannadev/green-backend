f18b89461a9df962d7632e05ba672d49f58c2853
alliwannadev <alliwannadev@gmail.com>
2025-05-13 13:41:00 +0900

    feat: Full-Text Index 관련 설정 변경 및 코드 추가 (#4)

---
+++ core/core-api/src/main/java/alliwannadev/shop/common/config/CustomFunctionContributor.java
@@ -0,0 +1,23 @@
+package alliwannadev.shop.common.config;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+import org.hibernate.type.StandardBasicTypes;
+
+public class CustomFunctionContributor implements FunctionContributor {
+
+    public static final int MATCH_THRESHOLD = 0;
+
+    @Override
+    public void contributeFunctions(FunctionContributions functionContributions) {
+        functionContributions.getFunctionRegistry()
+                .registerPattern(
+                        "MATCH_AGAINST",
+                        "MATCH (?1) AGAINST (?2 IN BOOLEAN MODE)",
+                        functionContributions
+                                .getTypeConfiguration()
+                                .getBasicTypeRegistry()
+                                .resolve(StandardBasicTypes.DOUBLE)
+                );
+    }
+}
