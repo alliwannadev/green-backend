45fbbd4d192b4d8d597f3f8984152ff6f54e6ed1
alliwannadev <alliwannadev@gmail.com>
2025-05-17 12:22:27 +0900

    feat: 장바구니 엔티티 및 리포지토리 추가 (#5)

---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/cart/model/CartItem.java
@@ -0,0 +1,108 @@
+package alliwannadev.shop.domain.cart.model;
+
+import alliwannadev.shop.supports.snowflake.GeneratedSnowflake;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CartItem {
+
+    @Id @GeneratedSnowflake
+    private Long cartItemId;
+
+    @JoinColumn(name = "CART_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cart cart;
+
+    private Long productId;
+
+    private String productName;
+
+    private String thumbnailUrl;
+
+    private Long price;
+
+    private Long quantity;
+
+    private Long amount;
+
+    private Long discountedAmount;
+
+    private Long paymentAmount;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public CartItem(
+            Cart cart,
+            Long productId,
+            String productName,
+            String thumbnailUrl,
+            Long quantity,
+            Long price,
+            Long amount,
+            Long discountedAmount,
+            Long paymentAmount
+    ) {
+        this.cart = cart;
+        this.productId = productId;
+        this.productName = productName;
+        this.thumbnailUrl = thumbnailUrl;
+        this.quantity = quantity;
+        this.price = price;
+        this.amount = amount;
+        this.discountedAmount = discountedAmount;
+        this.paymentAmount = paymentAmount;
+    }
+
+    public static CartItem of(
+            Cart cart,
+            Long productId,
+            String productName,
+            String thumbnailUrl,
+            Long price,
+            Long quantity,
+            Long amount,
+            Long discountedAmount
+    ) {
+        CartItem cartItem = CartItem.builder()
+                .productId(productId)
+                .productName(productName)
+                .thumbnailUrl(thumbnailUrl)
+                .price(price)
+                .quantity(quantity)
+                .amount(amount)
+                .discountedAmount(discountedAmount)
+                .paymentAmount(amount - discountedAmount)
+                .build();
+
+        cartItem.changeCart(cart);
+        return cartItem;
+    }
+
+    public void changeCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public void update(
+            Long price,
+            Long quantity,
+            Long amount,
+            Long discountedAmount
+    ) {
+        this.price = price;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.discountedAmount = discountedAmount;
+        this.paymentAmount = amount - discountedAmount;
+    }
+
+    public void updateQuantity(Long quantity) {
+        this.quantity = this.quantity + quantity;
+        this.amount = this.price * this.quantity;
+        this.paymentAmount = this.price * this.quantity - discountedAmount;
+    }
+}
---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/cart/repository/CartItemRepository.java
@@ -0,0 +1,34 @@
+package alliwannadev.shop.domain.cart.repository;
+
+import alliwannadev.shop.domain.cart.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query(
+            """
+            select  cartItem
+            from    CartItem cartItem
+            where   cartItem.cart.cartId = :cartId
+            """
+    )
+    List<CartItem> findAllByCartId(Long cartId);
+
+    @Query(
+            """
+            select  cartItem
+            from    CartItem cartItem
+            where   cartItem.cart.cartId = :cartId
+            and     cartItem.productId = :productId
+            """
+    )
+    Optional<CartItem> findOneByCartIdAndProductId(
+            Long cartId,
+            Long productId
+    );
+}
+
