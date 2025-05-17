3c7337d2c1db0f3e3d7c4cb4f842e6dcc5dd7932
alliwannadev <alliwannadev@gmail.com>
2025-05-17 17:58:43 +0900

    feat: 주문 API 구현 (#6)

---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/order/controller/OrderApiPaths.java
@@ -0,0 +1,8 @@
+package alliwannadev.shop.domain.order.controller;
+
+import alliwannadev.shop.common.constant.ApiPaths;
+
+public final class OrderApiPaths {
+
+    public static final String V1_ORDERS = ApiPaths.V1 + "/orders";
+}
