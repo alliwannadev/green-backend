b3b5bae703e20c2df4c4e1b6ca609600adeb8682
alliwannadev <alliwannadev@gmail.com>
2025-05-17 17:56:44 +0900

    feat: 주문 엔티티 및 리포지토리 추가 (#6)

---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/order/model/OrderItem.java
@@ -0,0 +1,98 @@
+package alliwannadev.shop.domain.order.model;
+
+import alliwannadev.shop.common.model.BaseTimeEntity;
+import alliwannadev.shop.supports.snowflake.GeneratedSnowflake;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderItem extends BaseTimeEntity {
+
+    @Id @GeneratedSnowflake
+    private Long orderItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    private Long productId;
+
+    private Long productOptionCombinationId;
+
+    private Long price;
+
+    private Long discountedPrice;
+
+    private Long paymentPrice;
+
+    private Long quantity;
+
+    private Long amount;
+
+    private Long discountedAmount;
+
+    private Long paymentAmount;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public OrderItem(
+            Long productId,
+            Long productOptionCombinationId,
+            Long price,
+            Long discountedPrice,
+            Long paymentPrice,
+            Long quantity,
+            Long amount,
+            Long discountedAmount,
+            Long paymentAmount
+    ) {
+        this.productId = productId;
+        this.productOptionCombinationId = productOptionCombinationId;
+        this.price = price;
+        this.discountedPrice = discountedPrice;
+        this.paymentPrice = paymentPrice;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.discountedAmount = discountedAmount;
+        this.paymentAmount = paymentAmount;
+    }
+
+    public static OrderItem of(
+            Order order,
+            Long productId,
+            Long productOptionCombinationId,
+            Long price,
+            Long discountedPrice,
+            Long paymentPrice,
+            Long quantity,
+            Long amount,
+            Long discountedAmount,
+            Long paymentAmount
+    ) {
+        OrderItem orderItem = OrderItem
+                .builder()
+                .productId(productId)
+                .productOptionCombinationId(productOptionCombinationId)
+                .price(price)
+                .discountedPrice(discountedPrice)
+                .paymentPrice(paymentPrice)
+                .quantity(quantity)
+                .amount(amount)
+                .discountedAmount(discountedAmount)
+                .paymentAmount(paymentAmount)
+                .build();
+
+        orderItem.changeOrder(order);
+
+        return orderItem;
+    }
+
+    public void changeOrder(Order order) {
+        this.order = order;
+        order.getOrderItems().add(this);
+    }
+}
---
+++ core/core-api/src/main/java/alliwannadev/shop/domain/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package alliwannadev.shop.domain.order.repository;
+
+import alliwannadev.shop.domain.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
