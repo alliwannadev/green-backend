6224b0b0a85390987932f08541e809968e306694
alliwannadev <alliwannadev@gmail.com>
2025-05-19 14:29:32 +0900

    feat: 도메인 모듈 생성 (#16)

---
+++ core/core-domain/src/main/java/alliwannadev/shop/core/domain/DomainConfig.java
@@ -0,0 +1,10 @@
+package alliwannadev.shop.core.domain;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("alliwannadev.shop.core.domain")
+@Configuration
+public class DomainConfig {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "green-backend"
 include(
+    "core:core-domain",
     "core:core-api",
     "core:core-consumer",
     "core:core-consumer:coupon-consumer",
